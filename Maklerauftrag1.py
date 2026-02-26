2a1c9663ed912ab429557a77f6cd468b558ca78f
Vivien Helders <vivien.h8@gso.schule.koeln>
2026-02-26 15:56:12 +0100

    Erste fertige Version Maklerauftrag

---
+++ Maklerauftrag1.py
@@ -0,0 +1,35 @@
+raeume = []
+gesamtflaeche= 0.0
+
+
+while True:
+    raum_name =input("Raumname oder ende: ")
+    
+    if raum_name.lower() == "ende":
+        break
+
+    try:
+        laenge = float(input(f"Länge {raum_name}: "))
+        breite = float(input(f"Breite {raum_name}: "))
+
+        flaeche = laenge * breite
+
+        raum_daten = {"name" : raum_name, "quadratmeter": flaeche}
+
+        raeume.append(raum_daten)
+        print(f" Raum: {raum_name} mit {flaeche: .2f} qm2.")
+    
+    except ValueError:
+        print("Bitte gib nur Zahlen (mit Punkt statt Komma) ein! \n")
+
+for raum in raeume: 
+    name = raum["name"]
+    qm2 = raum["quadratmeter"]
+
+    print(f"{name} Quadratmeter: {qm2}")
+
+    gesamtflaeche += qm2
+
+print(f"Gesamtfläche: {gesamtflaeche}qm2, Raumbezeichnung: {raum}")
+
+
