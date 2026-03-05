1337115d62469308c12551341ec0d02088f3e020
Vivien Helders <vivien.h8@gso.schule.koeln>
2026-03-05 10:51:32 +0100

    Übungen Datentypen, Listen & Schleifen

---
+++ uebung.py
@@ -0,0 +1,39 @@
+# Datentypen
+
+# String: "string"
+# integer:  40
+# float: 40.05
+# Boolean: False, True
+
+
+# 
+
+# Listen:
+
+variable1 = 1
+variable2 = 2
+
+Liste1 = [variable1, variable2]
+
+# Dictionary
+                # key     # value
+dictionary1 = {"name" : "Pragash"}
+
+# print(dictionary1["Pragash"])
+
+tupel1 = "wert"
+tupel2 = "wert2"
+
+tupel = (tupel1, tupel2)
+
+set = {"rot", "gelb", "grün", "rot"}
+
+
+for i in set:
+    print(i)
+
+
+boolean = False
+
+while boolean is True: 
+    print("hi")
